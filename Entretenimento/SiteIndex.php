7958212be773d0bf773cdb0204d3cf93965973c7
Daanilo-s19 <danilodelete@hotmail.com>
2018-11-14 17:41:07 -0300

    erro na busca do bd

--- Entretenimento/SiteIndex.php
+++ Entretenimento/SiteIndex.php
@@ -148,9 +148,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                         $query ="SELECT  * FROM midia";
                         $results = $db->selectDB($query);
                         //FOREACH PARA AGRUPAR TODOS OS OBJS DO GENERO AÇÃO EM UM VETOR 
-                        $filmes=[];  
+                        $filmes=[];                         
                         foreach($results as $filmes) {
-                            $obj= new Media($filmes["ident"], $filmes["tipo"], $filmes["genero"], $filmes["titulo"], $filmes["diretor"], $filmes["elenco"], $filmes["imagem"], $filmes["sinopse"], $filmes["ano"], $filmes["avaliacao"], $filmes["duracao"], $filmes["classificacao"]);
+                            $obj= new Media($filmes["indice"], $filmes["tipo"], $filmes["genero"], $filmes["titulo"], $filmes["diretor"], $filmes["elenco"], $filmes["imagem"], $filmes["sinopse"], $filmes["ano"], $filmes["avaliacao"], $filmes["duracao"], $filmes["classificacao"]);
                             $filmes[] = $obj;
                            
                         }
@@ -158,10 +158,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                          for($lista = 0; $lista<3; $lista++ ){ // O TOTAL DE LISTA É COM BASE NO NUMERO DE OBJETO DIVIDIDO POR 6
                             echo "  
                                 <li>
-                                    <div class=\"agile_tv_series_grid\">"; 
-                                      $filme = new Media();                                  
+                                    <div class=\"agile_tv_series_grid\">";                                                                       
                                       for ($i = 0; $i < 6; $i++) 
-                                          $filme->cartaz();
+                                      $filmes[$i]->cartaz();
                             echo "
                                     </div>
                                </li>";
