1f1a1b92c9e6a774080dbce84d33b41708eb83ea
Daanilo-s19 <danilodelete@hotmail.com>
2018-11-16 18:44:00 -0300

    cadastra usuario/ uso de hash p/ senha

---
+++ ValidarCadastro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require_once("PHP/dbcontroller.php");
+        
+         $db = new DBController();
+        
+        $login = $_POST["login"];
+        $senha = md5($_POST["senha"]); // HASHING VAGABUNDO
+        $email = $_POST["email"];
+        $datanasc = $_POST["datanasc"];
+        
+        $query = "INSERT INTO usuarios (login, senha, email,datanasc) VALUES ('$login', '$senha', '$email','$datanasc')";
+	if ($db->insertDB($query)== true){
+            setcookie("login", $login);
+            header("Location:Cadastro.php?sucesso=1");
+        }
+        else{
+            header("Location:Cadastro.php?sucesso=0");
+            
+        }
+	
+        ?>
+    </body>
+</html>
